13531a8963e1824405c0075d19f5c77d8969f0b8
CCat <ccathlete01@gmail.com>
2024-07-15 11:48:14 +0200

    Started working on the image processor API.

--- imageHandler/imageHandler_API.go
+++ imageHandler/imageHandler_API.go
@@ -1,1 +1,24 @@
 package imagehandler
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+func GetTextFromImages(convertedPdfDir string) error {
+	// Going over the pages (as jpegs) and preprocessing them.
+	dir, err1 := os.Open(convertedPdfDir)
+	files, err2 := os.ReadDir(convertedPdfDir)
+	if err1 != nil || err2 != nil {
+		return fmt.Errorf("couldn't go over files in dir %s: %v", convertedPdfDir, err)
+	}
+
+	for file := range files {
+		if filepath.Ext() == ".jpeg" {
+
+		}
+	}
+
+	return nil
+}
--- imageHandler/image_processor.go
+++ imageHandler/image_processor.go
@@ -2,7 +2,6 @@ package imagehandler
 
 import (
 	"fmt"
-	"log"
 	"os"
 	"path/filepath"
 
@@ -35,22 +34,29 @@ func ThresholdImage(image gocv.Mat) gocv.Mat {
 	return binaryImage
 }
 
-func ProcessImage(imagePath string, needProcessing bool) (outPath string) {
+func ProcessImage(imagePath string, needProcessing bool) (string, error) {
 	outDir := fmt.Sprintf("%s/AfterProcessing", filepath.Dir(imagePath))
-	outPath = fmt.Sprintf("%s/Processed_%s", outDir, filepath.Base(imagePath))
+	outPath := fmt.Sprintf("%s/Processed_%s", outDir, filepath.Base(imagePath))
+
+	// Creating the output dir if it doesn't exist. If it exists mkdir will get an error but it's ok for us because the dir exists.
+	// Note: I used isNotExists because it takes into account an empty file.
+	if err := os.MkdirAll(outDir, 0755); os.IsNotExist(err) {
+		// If the dir doesn't exist but we still got an error.
+		return "", fmt.Errorf("error when trying to create the output dir even"+
+			"though it doesn't already exist: %v", err)
+	}
 
 	if needProcessing {
 		// Loading the image.
 		beforeProcImage, err := LoadImage(imagePath)
 		if err != nil {
-			log.Fatalf("\nProblem with loading the image: %v", err)
+			return "", fmt.Errorf("\nProblem with loading the image: %v", err)
 		}
 
 		// Converting to grayscale.
 		grayImage := ConvertToGray(beforeProcImage)
 		if grayImage.Empty() {
 			fmt.Printf("Error when converting to grayscale the image at %s\n", imagePath)
-			return ""
 		}
 		defer grayImage.Close()
 
@@ -58,22 +64,13 @@ func ProcessImage(imagePath string, needProcessing bool) (outPath string) {
 		afterThresh := ThresholdImage(grayImage)
 		defer afterThresh.Close()
 
-		// Creating the output dir if it doesn't exist.
-		// Note: I used isNotExists because it takes into account an empty file.
-		if err := os.Mkdir(outDir, 0755); os.IsNotExist(err) {
-			// If the dir doesn't exist but we still got an error.
-			fmt.Printf("Error when trying to create the output dir even"+
-				"though it doesn't already exist: %v.\n", err)
-		}
-
 		// Saving the processed image.
 		if imageWasWritten := gocv.IMWrite(outPath, afterThresh); !imageWasWritten {
 			fmt.Printf("Error when writing the processed image: %s\n", outPath)
-			return ""
 		}
 
 		fmt.Printf("Successfully processed and saved the image to: %s\n", outPath)
 	}
 
-	return outPath
+	return outPath, nil
 }
